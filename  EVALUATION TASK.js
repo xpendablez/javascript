d61d783428290e1ecdc5a8ad4a6cfee24da15ef2
bharatchopkar <bharatc@xpendablez.com>
2017-05-11 08:25:31 +0530

    evaluation task solution

---  EVALUATION TASK.js
+++  EVALUATION TASK.js
@@ -10,8 +10,8 @@ console.log(fahrenheit);
 
 
 // task 3 Create a string with the name of your favorite sport. The first letter of the string should be capitalized.
-console.log("Swimming");
-
+var mySport="swimming";
+mySport.charAt(0).toUpperCase()+mySport.slice(1);
 
 // task 4 Fix the right side expression so it evaluates to true.
 var answer = "ALL Strings are CrEaTeD equal" == "ALL Strings are CrEaTeD equal";
@@ -27,30 +27,27 @@ console.log(answer);
 //task 6 Look at the picture below and identify which data type (Numbers, Strings, Booleans) would best represent the highlighted areas.
 LETTER                                                          TYPE
 
-A - NASA - National Aeronautics and Space Administration        strings                            
+A - NASA - National Aeronautics and Space Administration        string                           
 
 B - Apply now to go behind-the-scenes at a #NASASocial May 31-Jun 1, 2017 for the launch of SpaceX's #Dragon cargo spacecraft carrying science and supplies to the International Space Station: 
--                                                               strings
+-                                                               string
 
-C - http://go.nasa.gov/2qPSh9w                                   strings
+C - http://go.nasa.gov/2qPSh9w                                   string
 
 D - Whether something was "liked" or not.                        boolean
 
 
-E - 995                                                          numbers
+E - 995                                                          number
 
 
 //task 7 statement using concatenation
 var  thingOne= "red";
-var thingtwo="blue";
-var thingThree=thingOne.concat(thingtwo);
-console.log(thingThree);
-
+var thingTwo="blue";
+console.log(thingOne+" "+thingTwo);
 
 //task  8/1
 var fullName= "Bharat chopkar";
 console.log(fullName);
-
 
 //task 8/2
 var bill=10.25 + 3.99 + 7.15;
@@ -87,15 +84,15 @@ A dragon appears and attacks
 //task 11/1
 var num=2;
 if(num%2==0){
-	console.log("number :"+num+" is even.");
+	console.log("even");
 }else{
-	console.log("number :"+num+" is odd");
+	console.log("odd");
 }
 
 
 //task 11/2
 var musicians=4;
-if(musicians ==0){
+if(musicians <=0){
 	console.log( "not a group");
 }else if(musicians ==1){
 	console.log("solo");
@@ -116,20 +113,20 @@ var room = "gallery";
 var suspect = "Ms. Van Cleve";
 var weapon = "";
 var solved=false;
-if(room=="dining room", suspect=="Mr.Parkes"){
+if(room=="dining room" && suspect=="Mr.Parkes"){
 	solved=true;
 	weapon="knief";
-}else if(room=="gallery", suspect=="Ms. Van Cleve"){
+}else if(room=="gallery" && suspect=="Ms. Van Cleve"){
 	solved=true;
 	weapon="trophy";
-}else if(room=="billiards room", suspect=="Mrs. Sparr"){
+}else if(room=="billiards room" && suspect=="Mrs. Sparr"){
 	solved=true;
 	weapon="pool stick"
-}else if(room=="ballroom", suspect=="Mr. Kalehoff"){
+}else if(room=="ballroom" && suspect=="Mr. Kalehoff"){
 	solved=true;
 	weapon="poison";
-}if(solved=true){
-console.log("The"+suspect+" did it in the "+room+" with the "+weapon+".");
+}if(solved){
+console.log(suspect+" did it in the "+room+" with the "+weapon+".");
 }else{
 	console.log("murder mystery is unsolved");
 }
@@ -139,16 +136,18 @@ console.log("The"+suspect+" did it in the "+room+" with the "+weapon+".");
 var balance = 325.00;
 var checkBalance = true;
 var isActive = false;
-if(checkBalance = true){
-	if(isActive= true){
-		console.log("balance:"+balance);
-	}else if(isActive=false){
+if(checkBalance){
+	if(isActive && balance >0){
+		console.log("your balance is $"+balance.tofix+".");
+	}else if(!isActive){
 		console.log("your account is no longer active");
-	}else if(balance=0){
+	}else if(balance==0){
 		console.log("your account is empty");
+	}else {
+		console.log("your balance is in negative . please contact bank");
 	}
 }else{
-	console.log("your balance is negative pls contact bank");
+	console.log("Have a nice day Thank you");
 }
 
 
@@ -156,36 +155,69 @@ if(checkBalance = true){
 var flavor = " vanilla";
 var vessel = "cone";
 var toppings = "peanuts";
-if(flavor== "chocolate"||"vanilla"){
-	console.log("flavor :"+flavor);
-}else if(vessel=="cone"||"bowl"){
-	console.log("vessel :"+vessel);
-}else if(toppings=="sprinkles"||"peanuts"){
-	console.log("toppings :"+toppings);
-}else{
-	console.log("only plain ice cream");
+if((flavor == "vanilla" || flavor == "chocolate") && (vessel == "cone" || vessel == "bowl") && (toppings == "sprinkles" || toppings == "peanuts")) {
+	console.log("I'd like two scoops of "+flavor+" ice cream in a "+vessel+" with "+toppings+".")
 }
-console.log("I'd like two scoops of "+flavor+" ice cream in a "+vessel+" with "+toppings+".");
+
 //task 15
-
-
+var shirtWidth = 23;
+var shirtLength = 30;
+var shirtSleeve = 8.71;
+if((shirtWidth==18||shirtWidth==19)&&(shirtLength==28)&&(shirtSleeve==8.13||shirtSleeve<=8.37)){
+	console.log("s");
+}else if((shirtWidth==20||shirtWidth==21)&&(shirtLength==29)&&(shirtSleeve==8.38||shirtSleeve<=8.62)){
+	console.log("m");
+}else if((shirtWidth==22||shirtWidth==23)&&(shirtLength==30)&&(shirtSleeve==8.63||shirtSleeve<=8.87)){
+	console.log("l");
+}else if((shirtWidth==24||shirtWidth==25)&&(shirtLength==31||shirtLength==32)&&(shirtSleeve==8.88||shirtSleeve<=9.62)){
+	console.log("xl");
+}else if((shirtWidth==26||shirtWidth==27)&&(shirtLength==33)&&(shirtSleeve==9.63||shirtSleeve<=10.12)){
+	console.log("2xl");
+}else if((shirtWidth==28)&&(shirtLength==34)&&(shirtSleeve==10.13)){
+	console.log("3xl");
+}else{
+	console.log("not available");
+}
 
 
 //task 16
-var eatsPlants = true;
-var eatsAnimals = true;
-if(eatsAnimals==true){
-	console.log("carnivore");
-}else if(eatsPlants==true){
-	console.log("herbivore");
-}if(eatsPlants==true && eatsAnimals==true){
-	console.log("omnivore" );
-}else{
-	console.log("undefined");
-}
+var eatsPlants = false;
+var eatsAnimals = false;
 
+var category = "";
+
+if (eatsPlants && !eatsAnimals) {
+	category = "herbivore";
+} else if (eatsAnimals && !eatsPlants) {
+	category = "carnivore";
+} else if (eatsPlants && eatsAnimals) {
+	category = "omnivore";
+} else {
+	category = "undefined";
+}
+console.log(category);
 
 //task 17
+var education = "no high school diploma";
+var salary;
+if(education=="no high school diploma"){
+	console.log("In 2015, a person with "+education+" earned an average of "+salary+"/year.");
+}else if(education=="a high school diploma"){
+	console.log("In 2015, a person with "+education+" earned an average of "+salary+"/year.");
+}else if(education=="an Associate's degree"){
+	console.log("In 2015, a person with "+education+" earned an average of "+salary+"/year.");
+}else if(education=="a Bachelor's degree"){
+	console.log("In 2015, a person with "+education+" earned an average of "+salary+"/year.");
+}else if(education=="a Master's degree"){
+	console.log("In 2015, a person with "+education+" earned an average of "+salary+"/year.");
+}else if(education=="a Professional degree"){
+	console.log("In 2015, a person with "+education+" earned an average of "+salary+"/year.");
+}else{
+	console.log("In 2015, a person with a Doctoral degree earned an average of "+salary+"/year.");
+}
+switch (choice){
+	case 1:console.log(salary.toLocaleString("$25,636"));
+}
 
 
 
@@ -202,24 +234,38 @@ if(num % 3==0 && num % 5==0){
 	}else {
 			console.log(num);
 	}
-	num=num+1;
+	num+=1;
 }	
 
 //task 18/2 
-var num=99;
-while (num>=1){
+var num = 99;
 
+while(num >= 1) {
+	var bottle = (num>1)? "bottles" : "bottle";
+	console.log(num+" "+bottle+" of juice on the wall! "+num+" "+bottle+" of juice! Take one down, pass it around... "+(num-1)+" "+((num-1 > 1)? "bottles" : "bottle")+" of juice on the wall!");	
+	num--;
 }
 
-
-
-
-
-
-
-
-
-
+//task 18/3
+var time=60;
+while(time>=0){
+	if (time==50){
+		console.log("Orbiter transfers from ground to internal power.");
+	}else if(time==31){
+		console.log("Ground launch sequencer is go for auto sequence start.");
+	}else if(time==16){
+		console.log("Activate launch pad sound suppression system.");
+	}else if(time==10){
+		console.log("Activate main engine hydrogen burnoff system");
+	}else if(time==6){
+		console.log("Main engine start ");
+	}else if(time==0){
+		console.log("Solid rocket booster ignition and liftoff!");
+	}else{
+	console.log("T-minus "+time+" seconds.");
+	}
+	time--;
+}
 
 //task 19/1
 for (var i = 9;i >= 1 ; i--) {
@@ -237,6 +283,66 @@ for (var k = 0; k < 200; k++) {
 }
 
 //task 19/4
+var num=12;
+fact=1;
+for (var i=num;i<=num;i++) {
+	fact=fact*i
+}
+
+
+
+//task 19/5
+var seatNo="0-2";
+for (var i=0; i >= 25; i++) {
+	for (var j =1 ; i >= 99; i++) {
+		
+	}
+}
+
+
+//task 20/1
+function laugh(){
+	return "hahahahahahahahahaha!";
+}
+laugh();
+
+//task 20/2
+function laugh(num){
+	var num=1;
+	for (var i =1; i <=num ; i++) {
+		num+=i;
+	}
+	return "hi";
+}
+console.log(laugh(3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
