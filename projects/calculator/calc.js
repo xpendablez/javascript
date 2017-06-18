2dfe79ae42f44fbe20ed92d72be33884cfe6fb4d
bharatchopkar <bharatc@xpendablez.com>
2017-06-18 18:30:52 +0530

    projet calc update

--- projects/calculator/calc.js
+++ projects/calculator/calc.js
@@ -11,10 +11,15 @@ function mul(num1,num2){
 function div(num1,num2){
 	printResult(num1/num2);
 }
-function clearScreen(num1,num2){
-	printResult(reset(num1,num2))
+function clearText(num1,num2){
+	document.getElementById("num1").value="";
+	document.getElementById("num2").value="";
+	document.getElementById("num3").value="";
 }
-
+function clearScreen(num1,num2){
+	//printResult(num1=="",num2=="");
+	printResult("");
+}
 function printResult(result){
 	document.getElementById('num3').value=result
 }
